1b20dc9687b21f296ccd28eef323b9302109e4ac
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-08 18:59:05 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("size of a char: %d byte(s)\n", sizeof(a));
 	printf("size of a int: %d byte(s)\n", sizeof(b));
-	printf("size of a long int: %d byte(s)\n", sizeof(c));
-       	printf("size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("size of a longint: %d byte(s)\n", sizeof(c));
+       	printf("size of a longlongint: %d byte(s)\n", sizeof(d));
         printf("size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
