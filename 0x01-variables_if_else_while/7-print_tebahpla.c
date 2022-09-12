dc591541f11c23f5da6998021217214b45998435
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-12 16:23:11 -0700

    print tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase of alphabet in reverse
+ *
+ * Return: Always (okay)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
