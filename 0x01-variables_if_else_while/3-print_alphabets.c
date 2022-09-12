9d318a2329fd251ba23bd275f6fe9ee89359eb14
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-12 15:01:07 -0700

    print alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase and uppercase
+ *
+ * Return: Always (okay)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	retrun (0);
+}
