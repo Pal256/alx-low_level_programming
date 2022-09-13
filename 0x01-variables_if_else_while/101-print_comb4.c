e2841b24609b1c95c7ca26604dcd18f38256dbb2
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-12 18:24:58 -0700

    print comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,19 +16,19 @@ int main(void)
 
 			for (m = 2; m <= 9; m++)
 			{
-				if (m > p && P > i)
+			if (m > p && P > i)
+			{
+
+				putchar(i + '0');
+				putchar(p + '0');
+				putchar(m + '0');
+					if (i != 7)
 				{
 
-					putchar(i + '0');
-					putchar(p + '0');
-					putchar(m + '0');
-					if (i != 7)
-					{
-
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
+			}
 			}
 		}
 	}
