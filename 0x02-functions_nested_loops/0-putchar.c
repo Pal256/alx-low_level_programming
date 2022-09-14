6f9fedecd83744634bf1bf23e194afe1ac05cd68
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-14 13:54:09 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 
 /**
  * main - print putchar
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	_putchar('_');
+	_putchar(' ');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
