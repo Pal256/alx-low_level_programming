4ccf98311cf52895c86f03b01175ef93ca0f0544
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-19 17:49:17 -0700

    strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,14 @@
 /**
  * _strlen - return the length of a string
  * @s: string
- * Return: return length as integer;
+ * Return: 0
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
 
-	whlie (*(s + len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
