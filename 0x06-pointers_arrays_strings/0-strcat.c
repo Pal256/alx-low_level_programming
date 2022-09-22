0dc7654b9cf259dcbfdbb5fa12dd33cb0bec07da
okaforpaul256 <okaforpaul256@gmail.com>
2022-09-22 14:01:00 -0700

    strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,8 @@
 /**
  * _strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
- * @dest: string that will be appended
- * @src: string to be concatenated upon
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
  *
  * Return: return poiner to @dest
  */
